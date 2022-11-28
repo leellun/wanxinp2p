b41a7d82c4bca1e90c4af32d0c0c12f4daeb4ea4
leellun <leellun@sina.cn>
2022-11-28 18:05:29 +0800

    swagger2

---
+++ wanxin-component/swagger-spring-boot-starter/src/main/java/com/newland/wanxin/swagger2/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.newland.wanxin.swagger2;
+
+import com.newland.wanxin.swagger2.properties.SwaggerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableConfigurationProperties(SwaggerProperties.class)
+@EnableSwagger2
+public class SwaggerConfig {
+    @Autowired
+    private SwaggerProperties swaggerProperties;
+
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
+                .select()
+                // 要扫描的API(Controller)基础包
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInfo() {
+        Contact contact = new Contact(swaggerProperties.getConcat().getName(), swaggerProperties.getConcat().getUrl(), swaggerProperties.getConcat().getUrl());
+        return new ApiInfoBuilder()
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .contact(contact)
+                .version(swaggerProperties.getVersion()).build();
+    }
+}
---
+++ wanxin-component/swagger-spring-boot-starter/src/main/java/com/newland/wanxin/swagger2/properties/SwaggerProperties.java
@@ -0,0 +1,21 @@
+package com.newland.wanxin.swagger2.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "swagger2")
+@Data
+public class SwaggerProperties {
+    private String version;
+    private String title;
+    private String basePackage;
+    private String description;
+    private Concat concat;
+
+    @Data
+    public static class Concat {
+        private String name;
+        private String url;
+        private String email;
+    }
+}
